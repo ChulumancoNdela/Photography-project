11a77f09ae46b64a48d46c16d5a573e91b178e2f
ChulumancoNdela <chulumanco.ndela@capaciti.org.za>
2024-04-24 15:59:37 +0200

    services

--- function.js
+++ function.js
@@ -1,3 +1,22 @@
+function appendToDisplay(value) {
+  document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+
+function calculate() {
+  var expression = document.getElementById('display').value;
+  try {
+      var result = eval(expression);
+      document.getElementById('display').value = result;
+  } catch (error) {
+      document.getElementById('display').value = 'Error';
+  }
+}
+ 
+
 // JavaScript to toggle display of login and registration forms
 document.getElementById("loginLink").addEventListener("click", function() {
     document.getElementById("loginForm").style.display = "block";
@@ -7,4 +26,6 @@ document.getElementById("loginLink").addEventListener("click", function() {
 document.getElementById("registerLink").addEventListener("click", function() {
     document.getElementById("loginForm").style.display = "none";
     document.getElementById("registerForm").style.display = "block";
+
+
 });
